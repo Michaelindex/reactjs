0c803b510eaf5994de8034b972c77fd8b29740d1
Michael Santos <mcialbr123@gmail.com>
2023-07-06 20:18:15 -0300

    arrays

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import { Component } from 'react';
 
 class App extends Component{
   state = {
-      name : 'Michael Dos Santos',
-      counter : 0
+      posts: [
+        {
+          id: 1,
+          title: "O titulo 1",
+          body: "O corpo 1"
+
+        },
+        {
+          id: 2,
+          title: "O titulo 2",
+          body: "O corpo 2"
+
+        },
+        {
+          id: 3,
+          title: "O titulo 3",
+          body: "O corpo 3"
+
+        }
+      ]
     };
 
   render() {
     //const name = this.state.name
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     
     return (
