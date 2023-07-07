551b36c38a4ecab5229f9509b934c629800d9167
Michael Santos <mcialbr123@gmail.com>
2023-07-06 21:08:46 -0300

    ctrl z

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import { Component } from 'react';
 
 class App extends Component{
   state = {
-      posts: []
-  };
+      posts: [
+        {
+          id: 1,
+          title: "O titulo 1",
+          body: "O corpo 1"
 
-  componentDidMount() {
-    setTimeout(() => {    this.setState({
-      posts: [{
-        id: 1,
-        title: "O titulo 1",
-        body: "O corpo 1"
+        },
+        {
+          id: 2,
+          title: "O titulo 2",
+          body: "O corpo 2"
 
-      },
-      {
-        id: 2,
-        title: "O titulo 2",
-        body: "O corpo 2"
+        },
+        {
+          id: 3,
+          title: "O titulo 3",
+          body: "O corpo 3"
 
-      },
-      {
-        id: 3,
-        title: "O titulo 3",
-        body: "O corpo 3"
+        }
+      ]
+    };
 
-      }]
-    })}, 2000);
-  }
+    componentDidMount() {
+      console.log("oi")
+    }
 
   render() {
     //const name = this.state.name
