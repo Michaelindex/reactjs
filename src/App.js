a2cc57a7dd8e84ac5a52185963fde8e39240627e
Michael Santos <mcialbr123@gmail.com>
2023-07-06 21:13:26 -0300

    timeout

--- src/App.js
+++ src/App.js
@@ -3,39 +3,46 @@ import { Component } from 'react';
 
 class App extends Component{
   state = {
-      posts: [
-        {
-          id: 1,
-          title: "O titulo 1",
-          body: "O corpo 1"
+    counter : 0 ,
+    posts: [
+      {
+        id: 1,
+        title: "O titulo 1",
+        body: "O corpo 1"
 
-        },
-        {
-          id: 2,
-          title: "O titulo 2",
-          body: "O corpo 2"
+      },
+      {
+        id: 2,
+        title: "O titulo 2",
+        body: "O corpo 2"
 
-        },
-        {
-          id: 3,
-          title: "O titulo 3",
-          body: "O corpo 3"
+      },
+      {
+        id: 3,
+        title: "O titulo 3",
+        body: "O corpo 3"
 
-        }
-      ]
+      }
+    ]
     };
 
     componentDidMount() {
-      console.log("oi")
+      const { posts, counter } = this.state;
+      posts[0].title = "O Titulo Mudou";
+
+      setTimeout(() => {
+        this.setState({ posts });
+      }, 5000);
     }
 
   render() {
     //const name = this.state.name
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     
     return (
     <div className="App">
+      <p>{counter}</p>
       {posts.map(post => (
         <div  key={post.id} >
           <h1>{post.title}</h1>
