8c7062a2c880e2f790e0eec745f8b60a39dadd11
Michael Santos <mcialbr123@gmail.com>
2023-07-07 22:45:00 -0300

    key

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -2,7 +2,7 @@ export const PostCard = ({title, cover, body, id}) => {
   return (
     <div className='post'>
       <img src={cover} alt={title}/>
-      <div  key={id} className='post-content'>
+      <div className='post-content'>
         <h1>{title}</h1>
         <p>{body}</p>
       </div>/
